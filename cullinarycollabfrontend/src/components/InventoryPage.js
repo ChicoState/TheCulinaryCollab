46b0b7d491220f2cef3bec58d5af661c68442b41
Rguzman41 <rguzman4@csuchico.edu>
2023-11-13 12:27:29 -0800

    Updated Inventory Page

--- cullinarycollabfrontend/src/components/InventoryPage.js
+++ cullinarycollabfrontend/src/components/InventoryPage.js
@@ -1,7 +1,48 @@
 import React, { useState } from 'react';
 import './InventoryPage.css';
 
-const InventoryPage = () => {
+const RecipeFinder = ({ inventory }) => {
+    const [ingredients, setIngredients] = useState('');
+    const [recipes, setRecipes] = useState([]);
+
+    const handleIngredientsChange = (e) => {
+        setIngredients(e.target.value);
+    };
+
+    const findRecipes = () => {
+        const availableRecipes = [
+            { name: 'Recipe 1', ingredients: ['Ingredient A', 'Ingredient B'] },
+            { name: 'Recipe 2', ingredients: ['Ingredient B', 'Ingredient C'] },
+            // Add more recipes as needed
+        ];
+
+        const matchingRecipes = availableRecipes.filter(recipe =>
+            recipe.ingredients.every(ingredient => ingredients.includes(ingredient))
+        );
+
+        setRecipes(matchingRecipes);
+    };
+
+    return (
+        <div className="recipe-finder">
+            <h2>Find Recipes</h2>
+            <div className="form-group">
+                <label>Enter Ingredients (comma-separated):</label>
+                <input type="text" value={ingredients} onChange={handleIngredientsChange} />
+            </div>
+            <button type="button" onClick={findRecipes}>Find Recipes</button>
+
+            <h3>Matching Recipes:</h3>
+            <ul>
+                {recipes.map((recipe, index) => (
+                    <li key={index}>{recipe.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+const AddItemModel = ({ addItem, closeModel }) => {
     const [itemName, setItemName] = useState('');
     const [itemQuantity, setItemQuantity] = useState('');
     const [itemCategory, setItemCategory] = useState('');
@@ -20,40 +61,78 @@ const InventoryPage = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        addItem({
+            name: itemName,
+            quantity: itemQuantity,
+            category: itemCategory,
+        });
+        closeModel();
+    };
 
-        // Here you can send the form data to your backend API or perform any other necessary action.
-        // For now, let's just log the form data.
-        console.log('Item Name:', itemName);
-        console.log('Item Quantity:', itemQuantity);
-        console.log('Item Category:', itemCategory);
+    return (
+        <div className="model-overlay">
+            <div className="add-item-model">
+                <h2>Add Inventory Item</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label>Item Name:</label>
+                        <input type="text" value={itemName} onChange={handleItemNameChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label>Item Quantity:</label>
+                        <input type="number" value={itemQuantity} onChange={handleItemQuantityChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label>Item Category:</label>
+                        <input type="text" value={itemCategory} onChange={handleItemCategoryChange} required />
+                    </div>
+                    <button type="submit">Add Item</button>
+                </form>
+                <button className="close-model-button" onClick={closeModel}>
+                    Close
+                </button>
+            </div>
+        </div>
+    );
+};
 
-        // You can also clear the form fields after submission if needed.
-        setItemName('');
-        setItemQuantity('');
-        setItemCategory('');
+const SampleInventory = [
+    { name: 'Vodka', quantity: '1 bottle', category: 'Spirits' },
+    { name: 'Gin', quantity: '1 bottle', category: 'Spirits' },
+    { name: 'Lime Juice', quantity: '1 bottle', category: 'Juices' },
+];
+
+
+
+const InventoryPage = () => {
+    const [inventory, setInventory] = useState(SampleInventory);
+    const [isAddItemModelOpen, setAddItemModelOpen] = useState(false);
+
+    const addItem = (item) => {
+        setInventory([...inventory, item]);
+    };
+
+    const openAddItemModel = () => {
+        setAddItemModelOpen(true);
+    };
+
+    const closeAddItemModel = () => {
+        setAddItemModelOpen(false);
     };
 
     return (
         <div className="inventory-page">
-            <h2>Add Inventory Item</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label>Item Name:</label>
-                    <input type="text" value={itemName} onChange={handleItemNameChange} required />
-                </div>
-                <div className="form-group">
-                    <label>Item Quantity:</label>
-                    <input type="number" value={itemQuantity} onChange={handleItemQuantityChange} required />
-                </div>
-                <div className="form-group">
-                    <label>Item Category:</label>
-                    <input type="text" value={itemCategory} onChange={handleItemCategoryChange} required />
-                </div>
-                <button-1 type="submit">Add Item</button-1>
-            </form>
+            <h2>Inventory</h2>
+            <button onClick={openAddItemModel}>Add Item</button>
+	    <ul>{inventory.map((item, index) => (
+                    <li key={index}>{item.name} - {item.quantity} - {item.category}</li>
+                ))}</ul>
+            <RecipeFinder inventory={inventory} />
+            {isAddItemModelOpen && (
+                <AddItemModel addItem={addItem} closeModel={closeAddItemModel} />
+            )}
         </div>
     );
-}
+};
 
 export default InventoryPage;
-
--- cullinarycollabfrontend/src/components/LandingPage.js
+++ cullinarycollabfrontend/src/components/LandingPage.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function LandingPage() {
+    const [workshopRecipes, setWorkshopRecipes] = useState([]); // State to store workshop recipes
+
+    // Function to add a recipe to the workshop
+    const addRecipeToWorkshop = (recipe) => {
+        setWorkshopRecipes([...workshopRecipes, recipe]);
+    };
+
     return (
         <div className="landing-page-container">
             <div className="landing-content">
-                {}
+                <h2>Workshop Recipes</h2>
+                <ul>
+                    {workshopRecipes.map((recipe, index) => (
+                        <li key={index}>{recipe}</li>
+                    ))}
+                </ul>
             </div>
 
             <div className="about-button-container">
                 <Link to="/about">
                     <button className="about-button">About</button>
                 </Link>
+                {/* Button to add a recipe to the workshop */}
+                <button onClick={() => addRecipeToWorkshop("New Recipe")}>Add Recipe to Workshop</button>
+                {/* Button to share a recipe */}
+                <button onClick={() => alert("Recipe shared!")}>Share Recipe</button>
             </div>
         </div>
     );
