a1b9261050f50c29da22056500d8f39e4fc00242
Bryan Payne <bkpayne@csuchico.edu>
2023-12-07 17:10:01 -0800

    Functional Landing page w/infographic

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import {motion as m } from "framer-motion";
-
+import YellowInfographic from './Yellow_Landingpage_p2.png'; // Import the image
 function LandingPage() {
 	return (
 		<m.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.75}}>
 		<div className="landing-page-container">
 		<div className="landing-content">
 		{}
+		<img
+            src={YellowInfographic}
+            alt="Culinary Image"
+            className="landing-image"
+          />
 		</div>
 		<div className="about-content">
             		<h1>About Us</h1>
             		<div className="about-box">
-                		<p>In TheCullinaryCollab, we are just a small group of students who had an idea of being able to learn and share new drinks to anyone they want! We plan to allow users to share their creations with other users and be able to keep up with any and all recipies!</p>
+                		<p>In TheCullinaryCollab, we are a small community of students driven by the passion to learn and exchange new drink recipes with everyone! Our goal is to enable users to share their uniquely favorite creations and effortlessly access a variety of recipes within our platform</p>
             		</div>
         	</div>
 		</div>
